09a209cd9e981d770b5d126cec092c73c42a8f8a
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-01-29 18:15:50 +0100

    ownershipcomponene mobile finished animations left

--- src/components/HOME/Ownership.jsx
+++ src/components/HOME/Ownership.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Property from './Property'
+import "./Home.css"
 import appartment from "../../images/icon-apartment.png"
 
 const Ownership = () => {
@@ -11,35 +12,66 @@ const Ownership = () => {
       count:"123 Properties"
     },
     {
-      src:require('../../images/icon-apartment.png'),
+      src:require('../../images/icon-villa.png'),
       title:"Villa",
       count:"123 Properties"
     }
     , {
-      src:require('../../images/icon-apartment.png'),
+      src:require('../../images/icon-house.png'),
       title:"HOme",
+      count:"123 Properties"
+    },
+    {
+      src:require('../../images/icon-housing.png'),
+      title:"Office",
+      count:"123 Properties"
+    }
+    , {
+      src:require('../../images/icon-building.png'),
+      title:"BUilding",
+      count:"123 Properties"
+    },
+    {
+      src:require('../../images/icon-neighborhood.png'),
+      title:"Townhouse",
+      count:"123 Properties"
+    }
+    , {
+      src:require('../../images/icon-condominium.png'),
+      title:"Shop",
+      count:"123 Properties"
+    }
+    , {
+      src:require('../../images/icon-luxury.png'),
+      title:"Garage",
       count:"123 Properties"
     }
   ]
   return (
       
     <section className='ownaership' >
-    <div className="properties-section">
-    <h1 className="properties-title">Property Types</h1>
-    <p className="properties-content"> Get a variety of options as wild as the  imagination of your dream
-     apppartment could be , we got you covered with first class appartments all over the world at affordable prices    </p>
-    </div>
-   
-    {properties.map(property=>{
-        const{src,title, count} = property;
-        return(
-          <section>
-          {src}
-          {title}
-          {count}
-          </section>
-        )
-      })   } </section>
+      <div className="properties-section">
+        <h1 className="properties-title">Property Types</h1>
+        <p className="properties-content"> Get a variety of options as wild as the  imagination of your dream
+        apppartment could be , we got you covered with first class appartments all over the world at affordable prices    </p>
+      </div>
+    <div className='properties'>     
+      {properties.map(property=>{
+          const{src,title, count} = property;
+          return(
+            <section className='property' >
+            {/* This is an alternative way rodoir 
+            <div className="images"> <img src= {src}  />  </div>
+           <div className="title"> {title}  </div>
+           <div className="counr">  {count} </div>
+          */} 
+          
+          <Property src={src} title= {title}  count={count}  />
+            </section>
+          )
+        })   } 
+        </div>
+   </section>
   )
 }
 
--- src/components/HOME/Property.jsx
+++ src/components/HOME/Property.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-
+import "./Home.css"
 
 
 const Property = (props) => {
 
   return (
-    <div className='property'>
-     
-      <div className="property-image-div"> <img src={props.src} alt="property-img" className="propert-image" />  </div>
-      <div className="property-title-div"> <h3 className="property-title">{props.title}</h3></div>
-      <div className="property-count-div"><p className="properties-count"> {props.count} </p> </div>
+    <div className='property-prop'>
+      <div className="property-card"> 
+        <div className="property-image-div"> <img src={props.src} alt="property-img" className="propert-image" />  </div>
+        <div className="property-title-div"> <h3 className="property-title">{props.title}</h3></div>
+        <div className="property-count-div"><p className="properties-count"> {props.count} </p> </div>
+      </div>
     </div>
   )
 }
