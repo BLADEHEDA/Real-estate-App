bdb9ae6dc9732ac92091b36cad29f725cb82f6dd
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-02-02 22:43:33 +0100

    worst coding day of the year

--- src/components/SHARED/Footer.jsx
+++ src/components/SHARED/Footer.jsx
@@ -13,15 +13,15 @@ const Footer = () => {
     <h1 className="footer-touch-header">Get InTouch</h1>
     <footer className="footer-touch-info">
     <div className="touch-info">
-        <div className="touch-info-left"> <img src={map} alt="touch-image" className="touch-img" /> </div> 
+        <div className="touch-info-left"> <img src={map} alt="touch-" className="touch-img" /> </div> 
         <div className="touch-info-right"> </div> 
      </div>
         <div className="touch-info">
-            <div className="touch-info-left"> <img src={phone} alt="touch-image" className="touch-img" /> </div> 
+            <div className="touch-info-left"> <img src={phone} alt="touch-" className="touch-img-phone"/> </div> 
             <div className="touch-info-right"> </div> 
         </div>
   <div className="touch-info">
-  <div className="touch-info-left"> <img src={mail} alt="touch-image" className="touch-img" /> </div> 
+  <div className="touch-info-left"> <img src={mail} alt="touch-" className="touch-img-mail"/> </div> 
   <div className="touch-info-right"> </div> 
 </div>  
     </footer>
