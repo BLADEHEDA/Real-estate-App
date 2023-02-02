031de2760047fba2d868ede6231b48684d68dbbe
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-02-02 23:02:51 +0100

    worst coding day of the year

--- src/components/SHARED/Footer.jsx
+++ src/components/SHARED/Footer.jsx
@@ -12,17 +12,17 @@ const Footer = () => {
     <footer className="footer0touch">
     <h1 className="footer-touch-header">Get InTouch</h1>
     <footer className="footer-touch-info">
-    <div className="touch-info">
+    <div className="touch-info flex">
         <div className="touch-info-left"> <img src={map} alt="touch-" className="touch-img" /> </div> 
-        <div className="touch-info-right"> </div> 
+        <div className="touch-info-right">123 Street, New York, USA </div> 
      </div>
-        <div className="touch-info">
+        <div className="touch-info flex">
             <div className="touch-info-left"> <img src={phone} alt="touch-" className="touch-img-phone"/> </div> 
-            <div className="touch-info-right"> </div> 
-        </div>
-  <div className="touch-info">
+            <div className="touch-info-right">+012 345 67890 </div> 
+        </div> 
+  <div className="touch-info flex">
   <div className="touch-info-left"> <img src={mail} alt="touch-" className="touch-img-mail"/> </div> 
-  <div className="touch-info-right"> </div> 
+  <div className="touch-info-right">info@example.com </div> 
 </div>  
     </footer>
     </footer>
