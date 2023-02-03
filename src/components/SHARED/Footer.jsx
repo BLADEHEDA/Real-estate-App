b87eba7e615dc64055fa5a0d615872fae878c60a
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-02-03 13:14:32 +0100

    footer link section done

--- src/components/SHARED/Footer.jsx
+++ src/components/SHARED/Footer.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import phone from "../../images/icons8-phone-50.png"
 import map from "../../images/map-marker-2-16.png"
-import mail from "../../images/icons8-mail-30.png"
+import mail from "../../images/icons8-mail-30.png"  
+import facebook from "../../images/icons8-facebook-f-50.png"
+import linkedin from "../../images/icons8-linkedin-2-50.png"
+import twitter  from "../../images/icons8-twitter-50.png"
+import youtube  from "../../images/icons8-youtube-50.png"
+import arrowdown from "../../images/rTjr6pqxc.png"
+
 
 
 
@@ -9,24 +15,51 @@ import mail from "../../images/icons8-mail-30.png"
 const Footer = () => {
   return (
     <footer className='footer' >
-    <footer className="footer0touch">
-    <h1 className="footer-touch-header">Get InTouch</h1>
-    <footer className="footer-touch-info">
-    <div className="touch-info flex">
-        <div className="touch-info-left"> <img src={map} alt="touch-" className="touch-img" /> </div> 
-        <div className="touch-info-right">123 Street, New York, USA </div> 
-     </div>
-        <div className="touch-info flex">
-            <div className="touch-info-left"> <img src={phone} alt="touch-" className="touch-img-phone"/> </div> 
-            <div className="touch-info-right">+012 345 67890 </div> 
-        </div> 
-  <div className="touch-info flex">
-  <div className="touch-info-left"> <img src={mail} alt="touch-" className="touch-img-mail"/> </div> 
-  <div className="touch-info-right">info@example.com </div> 
-</div>  
+      <footer className="footer-touch">
+        <h1 className="footer-touch-header">Get InTouch</h1>
+        <footer className="footer-touch-info">
+            <div className="touch-info flex">
+                <div className="touch-info-left"> <img src={map} alt="touch-" className="touch-img" /> </div> 
+                <div className="touch-info-right">123 Street, New York, USA </div> 
+            </div>
+                <div className="touch-info flex">
+                    <div className="touch-info-left"> <img src={phone} alt="touch-" className="touch-img-phone"/> </div> 
+                    <div className="touch-info-right">+012 345 67890 </div> 
+                </div> 
+          <div className="touch-info flex">
+          <div className="touch-info-left"> <img src={mail} alt="touch-" className="touch-img-mail"/> </div> 
+          <div className="touch-info-right">info@example.com </div> 
+        </div>  
+        </footer>
+          <footer className="footer-iconss flex">  
+          <div className="fotter-icons8"> <img src={twitter } alt="footer-icons" className="footer-img" /> </div>
+          <div className="fotter-icons8"> <img src={facebook} alt="footer-icons" className="footer-img" /> </div>
+          <div className="fotter-icons8"> <img src={youtube} alt="footer-icons" className="footer-img" /> </div>
+          <div className="fotter-icons8"> <img src={linkedin} alt="footer-icons" className="footer-img" /> </div>
+          </footer>  
     </footer>
-    </footer>
-      
+
+      <footer className="footer-links"> </footer>
+        <div className="footer-link"> 
+          <div className="footer-link-arrow"><img src= {arrowdown } alt="arrow-down" className="footer-link-arrow-img" /> </div>
+          <div className="footer-link-div"> <a href="/#" className="footer-link">About Us</a> </div>
+        </div>
+        <div className="footer-link"> 
+          <div className="footer-link-arrow"><img src= {arrowdown } alt="arrow-down" className="footer-link-arrow-img" /> </div>
+          <div className="footer-link-div"> <a href="/" className="footer-link">Contact Us</a> </div>
+        </div>
+        <div className="footer-link"> 
+        <div className="footer-link-arrow"><img src= {arrowdown } alt="arrow-down" className="footer-link-arrow-img" /> </div>
+        <div className="footer-link-div"> <a href="/" className="footer-link">Our Services</a> </div>
+      </div>
+      <div className="footer-link"> 
+      <div className="footer-link-arrow"><img src= {arrowdown } alt="arrow-down" className="footer-link-arrow-img" /> </div>
+      <div className="footer-link-div"> <a href="/" className="footer-link">Privacy Policy</a> </div>
+    </div>
+    <div className="footer-link"> 
+    <div className="footer-link-arrow"><img src= {arrowdown } alt="arrow-down" className="footer-link-arrow-img" /> </div>
+    <div className="footer-link-div"> <a href="/" className="footer-link">Terms & Condtion</a> </div>
+  </div>
     </footer>
   )
 }
