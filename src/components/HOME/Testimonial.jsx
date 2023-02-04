eb2cffe9c1ae723a6b3bbf3289626f3badeffb12
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-02-04 22:20:17 +0100

    useless commit

--- src/components/HOME/Testimonial.jsx
+++ src/components/HOME/Testimonial.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import testinonial1 from '../../images/testimonial-1.jpg'
 import testinonial2 from '../../images/testimonial-2.jpg'
 import testinonial3 from '../../images/testimonial-3.jpg'
-import { Controller } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Carousel } from '@trendyol-js/react-carousel';
 import Button from '../SHARED/Button'
+import { useState } from "react";
+
+
 
 
 // let me create the testimony prop 
@@ -36,16 +36,52 @@ const Testimonial = () => {
 
   return (
     <div className='testimonial'>
-    
-   
+        <div className="testimonial-preview">
+            <h1 className="testimonial-header">Our Clients Say!</h1>
+            <p className="testimonial-content">Eirmod sed ipsum dolor sit rebum labore magna erat. 
+            Tempor ut dolore lorem kasd vero ipsum sit
+            eirmod sit. Ipsum diam justo sed rebum vero dolor duo. </p>
+        </div> 
 
-
+      
+        <div className="person-testimonials">
+            <div className="person-testimonial">     
+                <Person
+                src={testinonial1}
+                profession="Profession"
+                name="Client Name" 
+                content=" Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Fugiat sint quo repellat aperiam nisi tempore praesentium expedita libero, fugit,
+                non corporis harum provident velit dolores facilis cupiditate dicta? " 
+                />
+            </div>
+            <div className="person-testimonial">     
+                <Person
+                src={testinonial2}
+                profession="Profession"
+                name="Client Name" 
+                content=" Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Fugiat sint quo repellat aperiam nisi tempore praesentium expedita libero, fugit,
+                non corporis harum provident velit dolores facilis cupiditate dicta? " 
+                />
+            </div>
+            <div className="person-testimonial">     
+                <Person
+                src={testinonial3}
+                profession="Profession"
+                name="Client Name" 
+                content=" Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Fugiat sint quo repellat aperiam nisi tempore praesentium expedita libero, fugit,
+                non corporis harum provident velit dolores facilis cupiditate dicta? " 
+                />
+            </div>
+        </div>
     </div>
-    
-
-
 
   )
 }
+
+
+
 
 export default Testimonial
