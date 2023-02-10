5a310de80ccd783049eea57ba80d5424608c2a36
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-02-10 23:25:23 +0100

    done with the 404 error page

--- src/components/PAGES/Error.jsx
+++ src/components/PAGES/Error.jsx
@@ -4,6 +4,7 @@ import Button from '../SHARED/Button'
 import Main from '../SHARED/Main'
 import erroIcon from "../../images/icons8-error-100 (1).png"
 import"../ABOUT/About.css"
+import "../HOME/Home.css"
 
 const Error = () => {
   return (
@@ -16,7 +17,9 @@ const Error = () => {
         <h2 className="Error-head-2">Page Not FOund</h2>
         <p className="Error-content"> We’re sorry, the page you have looked for does not exist in our website! 
         Maybe go to our home page or try to use a search?</p>
-        <Button  title=" Go Back To Home" />
+        <Button  title=" Go Back To Home"
+        style={{ color:"white", padding:"1em 2em",borderRadius:"5px" }}
+        />
     </div>
     </div>
   )
