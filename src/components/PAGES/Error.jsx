22e31a36d544b547ff6d144d7f298d150e287af2
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-02-10 23:04:12 +0100

    my pc is disturbing

--- src/components/PAGES/Error.jsx
+++ src/components/PAGES/Error.jsx
@@ -10,12 +10,14 @@ const Error = () => {
     <div className='Error'>
       <Main title="404 Error" content="404 ERROR"/>
       <Formbar/>
-     <div className="Error-img-div"> <img src={erroIcon} alt="" className="Error-img" />   </div> 
-      <h1 className="Error-head">404</h1>
-      <h2 className="Error-head-2">Page Not FOund</h2>
-      <p className="Error-content"> We’re sorry, the page you have looked for does not exist in our website! 
-      Maybe go to our home page or try to use a search?</p>
-      <Button  title=" Go Back To Home" />
+      <div className="Error-div">  
+        <div className="Error-img-div"> <img src={erroIcon} alt="" className="Error-img" />   </div> 
+        <h1 className="Error-head">404</h1>
+        <h2 className="Error-head-2">Page Not FOund</h2>
+        <p className="Error-content"> We’re sorry, the page you have looked for does not exist in our website! 
+        Maybe go to our home page or try to use a search?</p>
+        <Button  title=" Go Back To Home" />
+    </div>
     </div>
   )
 }
