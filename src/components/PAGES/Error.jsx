def5ff0174b16f0786bd7dddf6b4ceaad09b306e
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-02-10 22:55:08 +0100

    404 error changes done

--- src/components/PAGES/Error.jsx
+++ src/components/PAGES/Error.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Formbar from '../HOME/Formbar'
+import Button from '../SHARED/Button'
 import Main from '../SHARED/Main'
+import erroIcon from "../../images/icons8-error-100 (1).png"
+import"../ABOUT/About.css"
 
 const Error = () => {
   return (
     <div className='Error'>
       <Main title="404 Error" content="404 ERROR"/>
       <Formbar/>
+     <div className="Error-img-div"> <img src={erroIcon} alt="" className="Error-img" />   </div> 
+      <h1 className="Error-head">404</h1>
+      <h2 className="Error-head-2">Page Not FOund</h2>
+      <p className="Error-content"> We’re sorry, the page you have looked for does not exist in our website! 
+      Maybe go to our home page or try to use a search?</p>
+      <Button  title=" Go Back To Home" />
     </div>
   )
 }
