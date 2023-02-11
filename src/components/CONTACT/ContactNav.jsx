83b40cac0ba50c72550479914c58cf5434055554
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-02-11 22:20:35 +0100

    incomplete changes doneto the contact componrntt

--- src/components/CONTACT/ContactNav.jsx
+++ src/components/CONTACT/ContactNav.jsx
@@ -5,6 +5,9 @@ import Navbar from '../SHARED/Navbar'
 import Footer from '../SHARED/Footer'
 import Formbar from '../HOME/Formbar'
 import Main from '../SHARED/Main'
+import mappin from "../../images/map-pin.png"
+import email from   "../../images/email-10-16.png" 
+import phone from   "../../images/phone-16.png" 
 
 const ContactNav = () => {
   return (
@@ -19,15 +22,17 @@ const ContactNav = () => {
         ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo. </p>
         <div className="contact-nav-info-div">  
         <div className="contactnav-info"> 
-          <div className="contactnav-info-img-div"><img src="" alt="" className="contact-info-img" /> </div>
+          <div className="contactnav-info-img-div"><img src={mappin} alt="street img"
+           className="contact-info-img location-img" /> </div>
           <div className="contact-info-content">123 Street, New York, USA</div>
         </div>
           <div className="contactnav-info"> 
-            <div className="contactnav-info-img-div"><img src="" alt="" className="contact-info-img" /> </div>
+            <div className="contactnav-info-img-div">
+              <img src={email} alt=" info img list-lication" className="contact-info-img" /> </div>
             <div className="contact-info-content">info@example.com</div>
           </div>
               <div className="contactnav-info"> 
-                <div className="contactnav-info-img-div"><img src="" alt="" className="contact-info-img" /> </div>
+                <div className="contactnav-info-img-div"><img src={phone} alt="phone-img" className="contact-info-img" /> </div>
                 <div className="contact-info-content">+012 345 6789</div>
             </div>
         </div>
