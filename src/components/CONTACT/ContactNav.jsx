e0390e52bc8de4da2c93174326601d8215fc6264
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-02-12 16:55:34 +0100

    finished the contact info section

--- src/components/CONTACT/ContactNav.jsx
+++ src/components/CONTACT/ContactNav.jsx
@@ -20,22 +20,30 @@ const ContactNav = () => {
        <p className="contact0nav-content"> Eirmod sed ipsum dolor sit rebum labore
         magna erat. Tempor ut dolore lorem kasd vero
         ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo. </p>
-        <div className="contact-nav-info-div">  
-        <div className="contactnav-info"> 
+        <section className="contact-nav-info-div">  
+        <section className="contactnav-info"> 
+        <article className="contact-nav-card"> 
           <div className="contactnav-info-img-div"><img src={mappin} alt="street img"
-           className="contact-info-img location-img" /> </div>
+           className="contact-info-img location-img" id='location-img-img' /> </div>
           <div className="contact-info-content">123 Street, New York, USA</div>
-        </div>
-          <div className="contactnav-info"> 
+          </article>
+        </section>
+          <section className="contactnav-info"> 
+          <article className="contact-nav-card">  
             <div className="contactnav-info-img-div">
-              <img src={email} alt=" info img list-lication" className="contact-info-img" /> </div>
-            <div className="contact-info-content">info@example.com</div>
-          </div>
-              <div className="contactnav-info"> 
-                <div className="contactnav-info-img-div"><img src={phone} alt="phone-img" className="contact-info-img" /> </div>
-                <div className="contact-info-content">+012 345 6789</div>
+                <img src={email} alt=" info img list-lication" className="contact-info-img email-img" /> 
             </div>
-        </div>
+            <div className="contact-info-content">info@example.com</div>
+            </article>
+          </section>
+              <section className="contactnav-info">  
+                <article className="contact-nav-card"> 
+                <div className="contactnav-info-img-div">
+                  <img src={phone} alt="phone-img" className="contact-info-img phone-img" /> </div>
+                <div className="contact-info-content">+012 345 6789</div>
+                </article>
+            </section>
+        </section>
 
        </div>
        <Footer/>
